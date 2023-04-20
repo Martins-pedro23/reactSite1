0ff094ba9f40897d817becf1c0a5ce6551507f5d
Martins-pedro23 <martinsdeoliveira.pedro2@gmail.com>
2023-04-20 20:38:31 -0300

    atualizado

--- projetReact/src/App.jsx
+++ projetReact/src/App.jsx
@@ -1,8 +1,18 @@
-import Titulo from "./Titulo"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Contato from "./pages/Contato"
+import Sobre from "./pages/Sobre"
+import Home from "./pages/Home"
+
 function App(){
     return(
       <div>
-        <Titulo cor="blue"/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/"  element={<Home/>}/>
+            <Route path="/sobre"  element={<Sobre/>}/>
+            <Route path="/contato"  element={<Contato/>}/>
+          </Routes>
+        </BrowserRouter>
       </div>
       ) 
       
